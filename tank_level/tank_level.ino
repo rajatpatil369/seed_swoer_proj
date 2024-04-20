32db9b7e5fd3f5468e2bdf36ce4650b2341b1346
Rajat Patil <57714patilrk@gmail.com>
2024-04-21 04:03:35 +0530

    logic for tank level monitoring and indication

--- tank_level/tank_level.ino
+++ tank_level/tank_level.ino
@@ -1,46 +1,70 @@
 const int trigPin = 8;    // common trig pin
 const int echoPinA = 9;   // hc-sr04 a
 const int echoPinB = 10;  // hc-sr04 b
-const int echoPinC = 11;  // hc-sr04 c
-const int echoPinD = 12;  // hc-sr04 d
 
-const int clockPin = 6;
+const int serDataPin = 6; // level lights
 const int latchPin = 5;
-const int dataPin = 4;
+const int clockPin = 4;
+
+const int buzzerPin = 3;
+
+const int lowerLimit = 2;
+const int upperLimit = 29;
+
+int calcDis(int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);  // reads the echoPin, returns the sound wave travel time in microseconds
+  int distance = (int) ((duration * 0.034) / 2);
+  return distance;
+}
 
 void setup() {
   pinMode(trigPin, OUTPUT);   // hc-sr04, a to d
   pinMode(echoPinA, INPUT);
   pinMode(echoPinB, INPUT);
-  pinMode(echoPinC, INPUT);
-  pinMode(echoPinD, INPUT); 
-  pinMode(clockPin, OUTPUT);  // 74hc595
+  pinMode(buzzerPin, OUTPUT);  
+  pinMode(serDataPin, OUTPUT);  // 74hc595
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(clockPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("setup done!");
 }
-
+int i = 0;
 void loop() {
-  long disA, disB, disC, disD;
-  disA = dist(echoPinA, trigPin);
-  disB = dist(echoPinB, trigPin);
-  disC = dist(echoPinC, trigPin);
-  disD = dist(echoPinD, trigPin);
-  Serial.print("disA="); Serial.print(disA);
-  Serial.print(", disB="); Serial.print(disB);
-  Serial.print(", disC="); Serial.print(disC);
-  Serial.print(", disD="); Serial.print(disD);
-}
-
-
-long dist(int echoPin, int trigPin) {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);  // Reads the echoPin, returns the sound wave travel time in microseconds
-  distance = duration * 0.034 / 2;
-  return distance;
+  int disA = calcDis(echoPinA);
+  delay(100);
+  int disB = calcDis(echoPinB);
+  delay(100);
+  int dis[] = {disA, disB};
+  digitalWrite(latchPin, LOW);
+  for (int i = 0; i < 2; ++i) {
+    int level = map(dis[i], lowerLimit, upperLimit, 7, 0);
+    byte serData = 0x00;
+    if (dis[i] >= lowerLimit && dis[i] <= upperLimit) {
+      for (int j = 0; j < 8; ++j) {
+        if (j < level) {
+          bitSet(serData, j);
+        }
+      }
+    } else {
+      serData = 0b01000001;
+    }
+    if (dis[i] <= (upperLimit + 3) && dis[i] >= (upperLimit - 3)) {
+      digitalWrite(buzzerPin, !digitalRead(buzzerPin));
+    } else {
+      digitalWrite(buzzerPin, LOW);
+    }
+    Serial.println(digitalRead(buzzerPin));
+    Serial.print("dis"); Serial.print((char) ('A'+i)); Serial.print('='); Serial.print(dis[i]);
+    Serial.print(", level="); Serial.print(level);
+    Serial.print(", serData="); Serial.println(serData, BIN);
+    shiftOut(serDataPin, clockPin, LSBFIRST, serData);
+    delay(200);
+  }
+  digitalWrite(latchPin, HIGH);
+  Serial.println("");
 }
